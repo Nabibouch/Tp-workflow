53e9dbb49c5076e7d231b71e9a9c4714414e1c8c
Nabibouch <lmrb.nabil@gmail.com>
2025-03-24 16:24:13 +0100

    mauvais port utilisé

--- app/db/db.js
+++ app/db/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectionDB = async () => {
     try {
-        await mongoose.connect("mongodb://admin:admin@localhost:27017/");
+        await mongoose.connect("mongodb://mongo:27017/mydb");
         console.log(`MongoDB est connecté`);
     } catch (error) {
         console.error(`Problème lors de la connection`, error);
